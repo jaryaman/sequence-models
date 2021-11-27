575c4d47e914c3713170dc3e4400328a20c31eb8
jaryaman <j.aryaman25@gmail.com>
2021-11-27 13:42:48 +0000

    Update

--- src/seq/enc_dec_attn/model.py
+++ src/seq/enc_dec_attn/model.py
@@ -2,6 +2,7 @@ import math
 from time import time
 from typing import Optional
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -53,6 +54,88 @@ def run_epoch(data_iter, model: 'EncoderDecoder', loss_compute, print_every=50):
     return math.exp(total_loss / float(total_tokens))
 
 
+def greedy_decode(model: 'EncoderDecoder', src, src_mask, src_lengths, max_len=100, sos_index=1, eos_index=None):
+    """Greedily decode a sentence by choosing the token with maximum probability at each step"""
+    with torch.no_grad():
+        encoder_hidden, encoder_final = model.encode(src, src_mask, src_lengths)
+        prev_y = torch.ones(1, 1).fill_(sos_index).type_as(src)
+        trg_mask = torch.ones_like(prev_y)
+
+    output = []
+    attention_scores = []
+    hidden = None
+
+    for i in range(max_len):
+        with torch.no_grad():
+            out, hidden, pre_output = model.decode(encoder_hidden, encoder_final, src_mask, prev_y, trg_mask, hidden)
+
+            # we predict from the pre-output layer, which is a combination of Decoder state, prev emb, and context
+            prob = model.generator(pre_output[:, -1])
+
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data.item()
+        output.append(next_word)
+        prev_y = torch.ones(1, 1).type_as(src).fill_(next_word)
+        attention_scores.append(model.decoder.attention.alphas.cpu().numpy())
+
+    output = np.array(output)
+
+    # cut off everything starting from </s>
+    # (only when eos_index provided)
+    if eos_index is not None:
+        first_eos = np.where(output == eos_index)[0]
+        if len(first_eos) > 0:
+            output = output[:first_eos[0]]
+
+    return output, np.concatenate(attention_scores, axis=1)
+
+
+def lookup_words(x, vocab=None):
+    if vocab is not None:
+        x = [vocab.itos[i] for i in x]
+
+    return [str(t) for t in x]
+
+
+def print_examples(example_iter, model: 'EncoderDecoder', n=2, max_len=100, sos_index=1, src_eos_index=None,
+                   trg_eos_index=None, src_vocab=None, trg_vocab=None):
+    model.eval()
+    count = 0
+    print()
+
+    if src_vocab is not None and trg_vocab is not None:
+        src_eos_index = src_vocab.stoi[EOS_TOKEN]
+        trg_sos_index = trg_vocab.stoi[SOS_TOKEN]
+        trg_eos_index = trg_vocab.stoi[EOS_TOKEN]
+    else:
+        src_eos_index = None
+        trg_sos_index = 1
+        trg_eos_index = None
+
+    for i, batch in enumerate(example_iter):
+        src = batch.src.cpu().numpy()[0, :]
+        trg = batch.trg_y.cpu().numpy()[0, :]
+
+        # remove </s> if present
+        src = src[:-1] if src[-1] == src_eos_index else src
+        trg = trg[:-1] if trg[-1] == trg_eos_index else trg
+
+        result, _ = greedy_decode(
+            model, batch.src, batch.src_mask, batch.src_lengths, max_len=max_len, sos_index=trg_sos_index,
+            eos_index=trg_eos_index
+        )
+
+        print("Example #%d" % (i + 1))
+        print("Src : ", " ".join(lookup_words(src, vocab=src_vocab)))
+        print("Trg : ", " ".join(lookup_words(trg, vocab=trg_vocab)))
+        print("Pred: ", " ".join(lookup_words(result, vocab=trg_vocab)))
+        print()
+
+        count += 1
+        if count == n:
+            break
+
+
 # *** classes ***
 class SimpleLossCompute:
     def __init__(self, generator, criterion, opt=None):
@@ -69,6 +152,10 @@ class SimpleLossCompute:
 
         if self.opt is not None:
             loss.backward()
+            self.opt.step()
+            self.opt.zero_grad()
+
+        return loss.data.item() * norm
 
 
 class EncoderDecoder(nn.Module):
--- src/seq/enc_dec_attn/synth.py
+++ src/seq/enc_dec_attn/synth.py
@@ -2,9 +2,13 @@
 Random symbol generator for source:target memorization task
 """
 
-import torch
+import matplotlib.pyplot as plt
 import numpy as np
+import torch
+import torch.nn as nn
+
 from seq import USE_CUDA
+from seq.enc_dec_attn.model import make_model, run_epoch, SimpleLossCompute, print_examples
 from seq.enc_dec_attn.parse import Batch
 
 
@@ -21,3 +25,54 @@ def data_gen(vocab_size=11, batch_size=16, num_batches=100, length=10, pad_index
         src_lengths = [length - 1] * batch_size
         trg_lengths = [length] * batch_size
         yield Batch((src, src_lengths), (trg, trg_lengths), pad_index=pad_index)
+
+
+def train_copy_task():
+    """Train the simple copy task."""
+    vocab_size = 11
+    criterion = nn.NLLLoss(reduction="sum", ignore_index=0)
+    model = make_model(vocab_size, vocab_size, emb_size=32, hidden_size=64)
+    optim = torch.optim.Adam(model.parameters(), lr=0.0003)
+    eval_data = list(data_gen(vocab_size=vocab_size, batch_size=1, num_batches=100))
+
+    dev_perplexities = []
+
+    if USE_CUDA:
+        model.cuda()
+
+    for epoch in range(10):
+        print("Epoch %d" % epoch)
+
+        # train
+        model.train()
+        data = data_gen(vocab_size=vocab_size, batch_size=32, num_batches=100)
+        run_epoch(data, model,
+                  SimpleLossCompute(model.generator, criterion, optim))
+
+        # evaluate
+        model.eval()
+        with torch.no_grad():
+            perplexity = run_epoch(eval_data, model,
+                                   SimpleLossCompute(model.generator, criterion, None))
+            print("Evaluation perplexity: %f" % perplexity)
+            dev_perplexities.append(perplexity)
+            print_examples(eval_data, model, n=2, max_len=9)
+
+    return dev_perplexities
+
+
+def plot_perplexity(perplexities):
+    """plot perplexities"""
+    plt.title("Perplexity per Epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Perplexity")
+    plt.plot(perplexities)
+
+
+def main():
+    dev_perplexities = train_copy_task()
+    plot_perplexity(dev_perplexities)
+
+
+if __name__ == '__main__':
+    main()
