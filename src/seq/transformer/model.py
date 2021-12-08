76afea72860ffd26d9de876d4d163fb84f548f85
jaryaman <j.aryaman25@gmail.com>
2021-12-08 08:25:05 +0000

    Update

--- src/seq/transformer/model.py
+++ src/seq/transformer/model.py
@@ -18,6 +18,28 @@ def clones(module: nn.Module, N: int):
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
 
 
+def subsequent_mask(size):
+    """Mask out subsequent positions. This blocks attending to future words during training."""
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
+
+
+def attention(query, key, value, mask=None, dropout=None):
+    """Compute 'Scaled Dot Product Attention'. This is a faster/more memory efficient alternative to additive attention
+    in Bahdanau. TODO: Understand this better. This omits the alignment model.
+    """
+    d_k = query.size(-1)
+    # scaling controls the variance of the dot-product, so we control the vanishing gradient problem
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e-9)
+    p_attn = F.softmax(scores, dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+
 # *** classes ***
 
 class EncoderDecoder(nn.Module):
@@ -175,3 +197,67 @@ class EncoderLayer(nn.Module):
         """Follow Figure 1 (left) for connections"""
         x = self.sublayer[0](x, lambda i: self.self_attn(i, i, i, mask))
         return self.sublayer[1](x, self.feed_forward)
+
+
+class Decoder(nn.Module):
+    """Generic N layer decoder with masking"""
+
+    def __init__(self, layer, N):
+        super().__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+class DecoderLayer(nn.Module):
+    """Decoder is made of self-attn, src-attn, and feed forward"""
+
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super().__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        """Follow Figure 1 (right) for connections"""
+        m = memory
+        x = self.sublayer[0](x, lambda i: self.self_attn(i, i, i, tgt_mask))
+        x = self.sublayer[1](x, lambda i: self.src_attn(i, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        super().__init__()
+        assert d_model % h == 0
+        # Assume d_v == d_k  # TODO: ?
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        """Implements Figure 2. TODO: Understand better"""
+        if mask is not None:
+            # Same mask applied to all h heads
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+
+        # 1) Do all the linear projections in batch from d_model => h x d_k
+        query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+                             for l, x in zip(self.linears, (query, key, value))
+                             ]
+
+        # 2) Apply attention on all the projected vectors in batch
+        x, self.attn = attention(query, key, value, mask, dropout=self.dropout)
+
+        # 3) "Concat" using a view and apply a final linear layer
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        return self.linears[-1](x)
